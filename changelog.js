a820a3eb6c5c42e92bc78d367c9c94c63e0ca257
niknal357 <46793548+niknal357@users.noreply.github.com>
2022-11-28 11:10:16 +0100

    Add files via upload

--- changelog.js
+++ changelog.js
@@ -1,29 +1,29 @@
 var new_features = [
-  "Added changelog",
-  "Added right-click support",
-  "Added timer",
+    "Added changelog",
+    "Added right-click support",
+    "Added timer",
 ];
 var bug_fixes = [
-  "Fixed game not detecting when you win on donut shaped boards",
+    "Fixed game not detecting when you win on donut shaped boards",
 ];
 var known_issues = [
-  "No leaderboards ¯\\_(ツ)_/¯",
-  "Changelog bugs out when there is nothing in a given category",
-  "Timer dies at the end of the game",
+    "No leaderboards ¯\\_(ツ)_/¯",
+    "Changelog bugs out when there is nothing in a given category",
+    "Timer dies at the end of the game",
 ];
 window.addEventListener("load", function () {
-  var txt = "";
-  txt += "New Features:";
-  for (var i = 0; i < new_features.length; i++) {
-    txt += "\n - " + new_features[i] + "";
-  }
-  txt += "\n\nBug Fixes:";
-  for (var i = 0; i < bug_fixes.length; i++) {
-    txt += "\n - " + bug_fixes[i] + "";
-  }
-  txt += "\n\nKnown Issues:";
-  for (var i = 0; i < known_issues.length; i++) {
-    txt += "\n - " + known_issues[i] + "";
-  }
-  document.getElementById("changelog").innerText = txt;
+    var txt = "";
+    txt += "New Features:";
+    for (var i = 0; i < new_features.length; i++) {
+        txt += "\n - " + new_features[i] + "";
+    }
+    txt += "\n\nBug Fixes:";
+    for (var i = 0; i < bug_fixes.length; i++) {
+        txt += "\n - " + bug_fixes[i] + "";
+    }
+    txt += "\n\nKnown Issues:";
+    for (var i = 0; i < known_issues.length; i++) {
+        txt += "\n - " + known_issues[i] + "";
+    }
+    document.getElementById("changelog").innerText = txt;
 });
